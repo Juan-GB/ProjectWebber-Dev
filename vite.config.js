bf32ac6a2aeef47bf584326a3a7f68d007e2797a
LinkGr <juangabrf@gmail.com>
2025-02-10 18:35:15 -0300

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-	plugins: [
+	base: ["/projectwebber-dev"],
+  plugins: [
     react(), 
     tailwindcss(),
   ],
